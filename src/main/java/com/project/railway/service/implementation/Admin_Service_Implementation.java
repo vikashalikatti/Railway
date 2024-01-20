dcc41b87c7c7423a2dfe03a4ac13b01a3230c98b
vikashalikatti <vikaslahalikatti117@gmail.com>
2024-01-20 12:55:31 +0530

    test

--- src/main/java/com/project/railway/service/implementation/Admin_Service_Implementation.java
+++ src/main/java/com/project/railway/service/implementation/Admin_Service_Implementation.java
@@ -86,7 +86,7 @@ public class Admin_Service_Implementation implements Admin_Service {
 				String location = InetAddress.getLocalHost().getHostAddress();
 				long expirationMillis = System.currentTimeMillis() + 3600000; // 1 hour in milliseconds
 				Date expirationDate = new Date(expirationMillis);
-				emailService.sendInfoEmail(admin, location);
+//				emailService.sendInfoEmail(admin, location);
 				String token = jwtUtil.generateToken_for_admin(userDetails, expirationDate);
 				structure.setData(token);
 				structure.setMessage("Login Success");
