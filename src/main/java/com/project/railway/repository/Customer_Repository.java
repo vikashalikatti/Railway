d28f904fe7d46faa426b64f496a6ed8bdc8085ff
vikashalikatti <vikaslahalikatti117@gmail.com>
2024-01-20 14:16:27 +0530

    update

---
+++ src/main/java/com/project/railway/repository/Customer_Repository.java
@@ -0,0 +1,13 @@
+package com.project.railway.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.railway.dto.Customer;
+
+public interface Customer_Repository extends JpaRepository<Customer, Integer> {
+
+	Customer findByEmail(String email);
+
+	Customer findByMobile(long phone_number);
+
+}
