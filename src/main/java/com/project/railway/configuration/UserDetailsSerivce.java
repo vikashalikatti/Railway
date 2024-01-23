344ccf988ac87c940fe19ea11f63dbee7f0cc96b
vikashalikatti <vikaslahalikatti117@gmail.com>
2024-01-24 00:21:53 +0530

    railway

--- src/main/java/com/project/railway/configuration/UserDetailsSerivce.java
+++ src/main/java/com/project/railway/configuration/UserDetailsSerivce.java
@@ -10,16 +10,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.project.railway.dto.Admin;
+import com.project.railway.dto.Customer;
 import com.project.railway.repository.Admin_Repository;
+import com.project.railway.repository.Customer_Repository;
 
 @Service
 public class UserDetailsSerivce implements UserDetailsService {
 
 	private Admin_Repository admin_Repository;
 
-	public UserDetailsSerivce(Admin_Repository admin_Repository) {
+	private Customer_Repository customer_Repository;
+
+	public UserDetailsSerivce(Admin_Repository admin_Repository, Customer_Repository customer_Repository) {
 		super();
 		this.admin_Repository = admin_Repository;
+		this.customer_Repository = customer_Repository;
 	}
 
 	@Override
@@ -31,6 +36,11 @@ public class UserDetailsSerivce implements UserDetailsService {
 					Collections.singletonList(new SimpleGrantedAuthority("admin")));
 		}
 
+		Customer customer = customer_Repository.findByEmail(email);
+		if (customer != null) {
+			return new User(customer.getEmail(), customer.getPassword(),
+					Collections.singletonList(new SimpleGrantedAuthority("customer")));
+		}
 		throw new UsernameNotFoundException("User not found: " + email);
 	}
 }
--- src/main/java/com/project/railway/controller/Customer_Controller.java
+++ src/main/java/com/project/railway/controller/Customer_Controller.java
@@ -9,18 +9,21 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.project.railway.dto.Admin;
 import com.project.railway.dto.Customer;
 import com.project.railway.helper.ResponseStructure;
 import com.project.railway.service.Customer_Service;
 
 import freemarker.template.MalformedTemplateNameException;
 import freemarker.template.TemplateException;
+import freemarker.template.TemplateNotFoundException;
 import jakarta.mail.internet.ParseException;
 
 @RestController
@@ -36,12 +39,11 @@ public class Customer_Controller {
 			@RequestPart MultipartFile pic) throws Exception {
 		return customer_Service.signup(customer, pic);
 	}
-
+	
 	@PostMapping("/login")
-	public ResponseEntity<ResponseStructure<Customer>> login(@RequestParam String email, @RequestParam String password)
-			throws FileNotFoundException, MalformedTemplateNameException, ParseException, IOException,
-			TemplateException {
-		return customer_Service.login(email, password);
+	public ResponseEntity<ResponseStructure<Customer>> login(@RequestParam String name, @RequestParam String password)
+			throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException {
+		return customer_Service.login(name, password);
 	}
 
 	@PostMapping("/verify_otp")
@@ -61,8 +63,8 @@ public class Customer_Controller {
 	
 	@PostMapping("/reset-password")
 	public ResponseEntity<ResponseStructure<Customer>> setpassword(@RequestParam String email,
-			@RequestParam String password) {
-		return customer_Service.setPassword(email, password);
+			@RequestParam String password,@RequestHeader("Bearer") String token) {
+		return customer_Service.setPassword(email, password,token);
 	}
 
 }
