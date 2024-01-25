3d88c0395ee1ad201bdb3361d4c8cca857d7ee44
vikashalikatti <vikaslahalikatti117@gmail.com>
2024-01-25 21:18:52 +0530

    railway

--- src/main/java/com/project/railway/dto/Route.java
+++ src/main/java/com/project/railway/dto/Route.java
@@ -1,15 +1,11 @@
 package com.project.railway.dto;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
@@ -26,8 +22,8 @@ public class Route {
 	private String endStation; // Corrected field name
 	private double distance;
 
-	@OneToMany(cascade = CascadeType.PERSIST)
-	private List<Station> stations;
+//	@OneToMany(cascade = CascadeType.PERSIST)
+//	private List<Station> stations;
 
 	@OneToOne
 	private Train train;
