c1b3b1577b6aa0766133b4a75c494608ee03d283
vikashalikatti <vikaslahalikatti117@gmail.com>
2024-01-27 04:09:01 +0530

    railway

--- src/main/java/com/project/railway/service/Admin_Service.java
+++ src/main/java/com/project/railway/service/Admin_Service.java
@@ -33,4 +33,6 @@ public interface Admin_Service {
 	ResponseEntity<ResponseStructure<Train>> addRoutesWithPrices(Route route, String token, int trainNo);
 
 	ResponseEntity<ResponseStructure<Train>> addSeats(Seat seat, String token, int trainNo);
+
+	ResponseEntity<ResponseStructure<Train>> updateSeat(Seat seat, String token, int train_no);
 }
