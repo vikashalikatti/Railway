9ba016c2ab5e92c7dc55f669a4b65861f6f0c9a7
Vikas L Halikatti <vikaslhalikatti117@gmail.com>
2024-02-01 02:03:37 +0530

    railway

--- src/main/java/com/project/railway/repository/Station_Repository.java
+++ src/main/java/com/project/railway/repository/Station_Repository.java
@@ -1,11 +1,13 @@
 package com.project.railway.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.railway.dto.Station;
 
 public interface Station_Repository extends JpaRepository<Station, Long> {
-	
-	Station findByStationName(String start);
+
+	List<Station> findByStationName(String start);
 
 }
